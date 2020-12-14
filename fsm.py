e19af299f7045ae818dbc83f966d6875b35ac47c
Korin <k777k777tw@gmail.com>
2020-12-15 02:01:18 +0800

    test

--- fsm.py
+++ fsm.py
@@ -7,23 +7,25 @@ class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
+    def on_enter_initial(self,event):
+        send_text_message(reply_token, "HELLO")
+
     def is_going_to_state1(self, event):
         text = event.message.text
         return text.lower() == "go to state1"
 
-    def is_going_to_state2(self, event):
-        text = event.message.text
-        return text.lower() == "go to state2"
-
     def on_enter_state1(self, event):
         print("I'm entering state1")
-
         reply_token = event.reply_token
         send_text_message(reply_token, "Trigger state1")
         self.go_back()
 
     def on_exit_state1(self):
         print("Leaving state1")
+
+    def is_going_to_state2(self, event):
+        text = event.message.text
+        return text.lower() == "go to state2"
 
     def on_enter_state2(self, event):
         print("I'm entering state2")
